32ff9cad49d88d78e50d17f101542f886063b674
william cardoso <williancard123@gmail.com>
2022-05-20 15:14:03 -0400

    .

--- aula4/fatorialRecursivo.cpp
+++ aula4/fatorialRecursivo.cpp
@@ -1,13 +1,15 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-long int fatorial(int n){
-  if(n == 0)
+long int fatorial(int n)
+{
+  if (n == 0)
     return 1;
   else
-    return n * fatorial(n-1);
+    return n * fatorial(n - 1);
 }
-main() {
-  long int i = fatorial(10);
+main()
+{
+  long int i = fatorial(5);
   printf("fatorial %d", i);
 }
